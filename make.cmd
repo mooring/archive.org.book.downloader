026d04a367f8050ff2968ee6f0f770f9b08a484b
mooring <mooring@live.com>
2023-03-02 11:27:45 +0800

    optimize make.cmd variables

--- make.cmd
+++ make.cmd
@@ -1,15 +1,22 @@
 @echo off
-@where cl 2>&1 1>NUL
+set obj_folder=%~dp0obj
+set bin_folder=%~dp0demo
+where cl >NUL 2>&1 1>NUL 
 if not %errorlevel% equ 0 (
-    echo "please run this in develop prompt environment"
+    echo please run %~nx0 in develop prompt environment
     goto :EOF
 )
-:all
-if not exist obj mkdir obj
+:compile
 echo compiling the downloader
-@cl /nologo /W2 /Foobj/get.obj /Fedemo/get.exe src/get.c
+if not exist %obj_folder% mkdir %obj_folder%
+if not exist %bin_folder% mkdir %bin_folder%
+if not exist src/get.c (
+    echo please put %~nx0 at the project root folder
+    goto :EOF
+)
+cl /nologo /W2 /Fo%obj_folder%\get.obj /Fe%bin_folder%/get.exe src/get.c
 if %errorlevel% equ 0 (
-    echo well done & echo.get.exe has compiled in demo folder
+    echo well done & echo.get.exe has compiled in %bin_folder:*demo=demo% folder
 ) else (
     echo bad news & echo.
 )
